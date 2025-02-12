af8f4039914d45d3028f9875ee2032fc63ca6e3d
AtharvGaur <atharava112@gmail.com>
2025-02-12 12:42:43 +0530

    resolved layout files

--- app/forum/[slug]/page.tsx
+++ app/forum/[slug]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+
 import ChatForum from '@/components/ChatForum';
 
 const page = async ({ params }) => {
--- app/forum/layout.tsx
+++ app/forum/layout.tsx
@@ -31,10 +31,10 @@ export default function RootLayout({
         <body
           className={` antialiased`}
         >
-
-            <Navbar />
-            {children}
             
+            {/* <Navbar /> */}
+            {children}
+            <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
         </body>
       </html>
     </ClerkProvider>
