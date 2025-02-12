a46f226a07d667553208be83121e882cf9813c0e
AtharvGaur <atharava112@gmail.com>
2025-02-12 12:32:27 +0530

    Created layout in forums

--- app/forum/[slug]/page.tsx
+++ app/forum/[slug]/page.tsx
@@ -4,7 +4,9 @@ import ChatForum from '@/components/ChatForum';
 const page = async ({ params }) => {
   const slug = (await params).slug;
   return (
+    <div className='p-10'>
     <ChatForum></ChatForum>
+    </div>
   )
 }
 
--- components/ChatForum/ChatForum.tsx
+++ components/ChatForum/ChatForum.tsx
@@ -1,5 +1,7 @@
 "use client"
-import { Chat, useCreateChatClient } from 'stream-chat-react';
+import { useEffect, useState } from 'react';
+import { Channel, ChannelHeader, Chat, MessageInput, MessageList, Thread, useCreateChatClient, Window } from 'stream-chat-react';
+import type { User, Channel as StreamChannel } from 'stream-chat';
 
 // your Stream app information
 const apiKey = 'dz5f4d5kzrue';
@@ -7,16 +9,47 @@ const userId = 'wandering-waterfall-9';
 const userName = 'wandering';
 const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoid2FuZGVyaW5nLXdhdGVyZmFsbC05IiwiZXhwIjoxNzM5MzU5Mjc5fQ.vSIyIYwCOJNKtdsP6_Dqfryp3cBS0gjZCE5qsCypgaQ';
 
-const ChatForum = async () => {
+const user: User = {
+  id: userId,
+  name: userName,
+  image: `https://getstream.io/random_png/?name=${userName}`,
+};
+
+const ChatForum = () => {
+
+  const [channel, setChannel] = useState<StreamChannel>();
   const client = useCreateChatClient({
     apiKey,
     tokenOrProvider: userToken,
-    userData: { id: userId },
+    userData: user,
   });
+
+  useEffect(() => {
+    if (!client) return;
+
+    const channel = client.channel('messaging', 'custom_channel_id', {
+      image: 'https://getstream.io/random_png/?name=react',
+      name: 'Talk about React',
+      members: [userId],
+    });
+
+    setChannel(channel);
+  }, [client]);
 
   if (!client) return <div>Setting up client & connection...</div>;
 
-  return <Chat client={client}>Chat with client is ready!</Chat>
+  return  (
+    <Chat client={client}>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
 }
 
 export default ChatForum
